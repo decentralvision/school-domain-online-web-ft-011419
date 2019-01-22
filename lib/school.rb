beafb7c332b9e4eb6ed02948a62bb352f196203b
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-22 21:26:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,6 +13,6 @@ class School
     @roster[num]
   end
   def sort
-    @roster.collect {|grade, students| self[grade] = students.sort}
+    @roster.collect {|grade, students| self.@roster[grade] = students.sort}
   end
 end
